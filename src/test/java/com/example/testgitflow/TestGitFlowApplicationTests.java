4c54ab8abd21b3ad7769736ac205b01ffafe5d38
Zir Pav <zir-pav@mail.ru>
2022-05-23 10:55:19 +0300

    Initial commit

---
+++ src/test/java/com/example/testgitflow/TestGitFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testgitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
